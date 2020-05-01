adca20f0a014c8b64078c1b49b809fc8e4e7528d
Yasutaka Shinzaki <shinzaki@yasu26.tech>
2020-05-01 12:05:00 +0900

    #5 modify where require aws-sdk

--- aws-sdk.js
+++ aws-sdk.js
@@ -15,10 +15,9 @@
  **/
 
 module.exports = function(RED) {
-	"use strict";	
+	"use strict";
 	var util = require("util");
 	var vm = require("vm");
-	var AWS = require('aws-sdk');
 
 	function AWSConfigSetup(n) {
 		RED.nodes.createNode(this, n);
@@ -111,7 +110,7 @@ module.exports = function(RED) {
 			callback : function(results) {
 				sendResults(node, node.name, results);
 			},
-			AWS : AWS,
+			AWS : require('aws-sdk'),
 			console : console,
 			util : util,
 			Buffer : Buffer,
@@ -150,7 +149,7 @@ module.exports = function(RED) {
 			this.on("input", function(msg) {
 				try {
 					var start = process.hrtime();
-					context.msg = msg;					
+					context.msg = msg;
 					node.script.runInContext(context);
 					sendResults(node, node.name, context.results);
 
